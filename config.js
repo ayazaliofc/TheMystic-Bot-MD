058b8e992cc142742da1c5163c87c2f2e4b8e375
ɳoɓꀤʈɑ 👑🖤🎧 <164648341+ayazaliofc@users.noreply.github.com>
2024-11-19 13:52:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from 'url';
 import fs from 'fs'; 
 import moment from 'moment-timezone';
 
-global.botnumber = "+12139865493"
+global.botnumber = "12139865493"
 global.confirmCode = ""
 global.authFile = `MysticSession`;
 
